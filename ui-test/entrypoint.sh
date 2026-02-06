fb664f4264e1a1aab231f451768bea3c2096ec7e
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2026-02-06 11:34:51 +0530

    Update entrypoint.sh
    Signed-off-by: bhumi46 <111699703+bhumi46@users.noreply.github.com>

--- ui-test/entrypoint.sh
+++ ui-test/entrypoint.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
-echo "Listing files in /home/mosip/:"
-ls -l /home/mosip/
+set -e
 
-ls -l /home/mosip/
-echo "Listing files in /home/mosip/:"
+BASE_DIR=/home/inji
 
-echo "Listing feature files in /home/mosip/featurefiles/:"
-ls -l /home/mosip/featurefiles/
+echo "Listing files in $BASE_DIR:"
+ls -l $BASE_DIR
 
-echo "/home/mosip/src/test/resources/config/"
-ls -l /home/mosip/src/test/resources/config/
+echo "Listing feature files:"
+ls -l $BASE_DIR/featurefiles/
 
-echo "/home/mosip/test-output/SparkReport/"
-ls -l /home/mosip/test-output/SparkReport/
+echo "$BASE_DIR/src/test/resources/config/"
+ls -l $BASE_DIR/src/test/resources/config/
 
-echo "/home/mosip/src/test/resources/extent.properties"
-ls -l /home/mosip/src/test/resources/extent.properties
+echo "$BASE_DIR/test-output/SparkReport/"
+ls -l $BASE_DIR/test-output/SparkReport/
 
-echo "/home/mosip/src/"
-ls -l /home/mosip/src/
+echo "$BASE_DIR/src/test/resources/extent.properties"
+ls -l $BASE_DIR/src/test/resources/extent.properties || true
 
-echo "/home/mosip/src/main/"
-ls -l /home/mosip/src/main/
+echo "$BASE_DIR/src/"
+ls -l $BASE_DIR/src/
 
-echo "/home/mosip/src/main/java/"
-ls -l /home/mosip/src/main/java/
-
-echo "/home/mosip/src/main/java/utils/"
-ls -l /home/mosip/src/main/java/utils/
+echo "$BASE_DIR/src/main/java/utils/"
+ls -l $BASE_DIR/src/main/java/utils/
 
 java --version
-java -jar -Dmodules="$MODULES" -Denv.user="$ENV_USER" -Denv.endpoint="$ENV_ENDPOINT" -Denv.testLevel="$ENV_TESTLEVEL" uitest-injiverify-*.jar
+
+java -jar \
+  -Dmodules="$MODULES" \
+  -Denv.user="$ENV_USER" \
+  -Denv.endpoint="$ENV_ENDPOINT" \
+  -Denv.testLevel="$ENV_TESTLEVEL" \
+  $BASE_DIR/uitest-injiverify-*.jar
